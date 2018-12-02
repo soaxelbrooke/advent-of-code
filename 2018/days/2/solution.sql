9d90ce12a177370a19263c0f5c2914823796b389
Stuart Axelbrooke <stuart@axelbrooke.com>
2018-12-02 16:35:37 +0900

    Add day 2 part 2 solution

--- 2018/days/2/solution.sql
+++ 2018/days/2/solution.sql
@@ -1,4 +1,6 @@
 
+select count(*) from input;
+
 with id_letters as (
   select id, regexp_split_to_table(id, '') letter from input
 ), id_letter_counts as (
@@ -13,3 +15,35 @@ with id_letters as (
 )
 select count(*) filter (where has_double) * count(*) filter (where has_triple) as checksum
 from id_stats;
+
+
+
+with id_letters as (
+  select
+    (row_number() over ()) - idx_offset as idx,
+    id,
+    letter
+  from (
+    select id, regexp_split_to_table(id, '') as letter, sum(length(id)) over (order by id) as idx_offset from input
+    order by id
+  ) tmp_id_letters
+),
+letter_distances as (
+  select
+    left_id.id as left_id,
+    right_id.id as right_id,
+    sum(cast(left_id.letter != right_id.letter as integer)) as letter_distance
+  from id_letters left_id
+    full outer join id_letters right_id
+    on left_id.idx = right_id.idx and left_id.id != right_id.id
+  group by left_id, right_id
+  order by letter_distance asc
+  limit 1
+),
+mismatched_ids as (
+  select
+    regexp_split_to_table(left_id, '') as left_letter,
+    regexp_split_to_table(right_id, '') as right_letter
+  from letter_distances
+)
+select string_agg(left_letter, '') as answer from mismatched_ids where left_letter = right_letter;
